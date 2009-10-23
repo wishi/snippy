80c2eb10c62798e5343fc42d2cebd4eb7a0a1001
Marius <wishinet@gmail.com>
2009-10-23 10:30:08 +0200

    added cross_refs IDA script

---
+++ Python/IDA/cross_ref.py
@@ -0,0 +1,36 @@
+"""
+finds potencially vulnerable functions in binaries
+
+@DEPS:
+   * IDA >= 4.9 (http://www.hex-rays.com/idapro/idadown.htm)
+   * IDAPython http://d-dome.net/idapython/,
+               http://code.google.com/p/idapython/downloads/list
+   * Python >= 2.5
+
+- cross_ref is by Justin Seitz from Gray Hat Python. 
+All changes made a fairly
+trivial. 
+- depending on your target you can go after MS SDL baned functions
+(http://msdn.microsoft.com/en-us/library/bb288454.aspx)
+- tested against macho and PE on MacOS and Windows
+
+-- wishi
+"""
+
+from idaapi import *
+
+danger_funcs = ["strcpy", "sprintf", "memcpy"] # add more
+
+for funcs in danger_funcs:
+   addr = LocByName( func )
+   if addr != BADADDR:
+      cross_refs = CodeRefsTo( addr, 0 )
+      # grab cross-references to this address
+      print "Cross References to %s" %func
+      print "---------------------­------"
+      for ref in cross_refs:
+         print "%08x" %ref
+         # color the call red
+         SetColor( ref, CIC_ITEM, 0x0000ff)
+
+
