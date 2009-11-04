5ec69735c030d7eac39dc544ae95a21b2035d771
Marius <wishinet@gmail.com>
2009-11-04 15:21:51 +0100

    corrected my F-Sharp errors

--- F-Sharp/hello.fs
+++ F-Sharp/hello.fs
@@ -11,18 +11,11 @@ open System
 
 [<EntryPoint>]
 let main (args : string[])=
-   // if args.Length <> 2 then
-        //   failwith "Expected arguments <greeting> and <thing>"
-        
-        let greeting = args.[0]
-        let thing = args.[1]
-        
-        // let timeOfDay = DateTime.ToString("hh:mm:tt")
-        
-        printfn "%s, %s at" greeting thing // timeOfDay
-        
-        // exit
-        0        
-        
-        
-        
+    if (args.Length <> 2) then
+        failwith "Expected arguments <greeting> and <thing>"
+    let greeting = args.[0]
+    let thing = args.[1]
+    let timeOfDay = DateTime.Today.ToString()
+    printfn "%s, %s at %s" greeting thing timeOfDay
+    // exit
+    0        
