5ed891c1cf29d5e8ecc4f5b929651b3bf5ecff4f
Marius <wishinet@gmail.com>
2009-11-09 10:04:16 +0100

    delegates in F# stuff

---
+++ F-Sharp/Delegates/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpDemos
+{
+	class Program
+	{	
+		static void Main(string[] args)
+		{
+      // Using variables
+			Random rnd = new Random();
+			int num = 0;
+			num = num + rnd.Next(10);
+			num = num + rnd.Next(10);
+
+			Console.WriteLine("Result: {0}", num);
+
+			// Using immutable values (functional style)
+			int num0 = 0;
+			int num1 = num0 + rnd.Next(10);
+			int num2 = num1 + rnd.Next(10);
+
+			Console.WriteLine("Result: {0}", num);
+
+			// Summing & multiplying numbers in the functional style:
+			Console.WriteLine(SumNumbersF(1, 5));
+			Console.WriteLine(MulNumbersF(1, 5));
+
+      // General 'aggregation' function
+			Console.WriteLine(AggregateNumbers(0, (a, b) => a + b, 1, 5));
+			Console.WriteLine(AggregateNumbers(1, (a, b) => a * b, 1, 5));
+		}
+
+		static int SumNumbersF(int from, int to)
+		{
+			if (from > to) 
+				return 0;
+			else 
+				return from + SumNumbersF(from + 1, to);
+		}
+
+		static int MulNumbersF(int from, int to)
+		{
+			if (from > to)
+				return 1;
+			else
+				return from * MulNumbersF(from + 1, to);
+		}
+
+    // Using function (delegate 'op') as an argument, we can 
+    // 'hide' the difficult recursive behavior of the previous
+    // two methods into a single generally useful method
+		static int AggregateNumbers(int initial, Func<int, int, int> op, int from, int to)
+		{
+			if (from > to)
+				return initial;
+			else
+				return op(from, AggregateNumbers(initial, op, from + 1, to));
+		}
+	}
+}
---
+++ F-Sharp/Delegates/fsharp.fsx
@@ -0,0 +1,33 @@
+﻿#light
+
+// Declaring values using the 'let' keyword
+let hello = "Hello world"
+let num = 42
+
+// Declaring functions
+let twoTimes n = 
+  n * 2
+
+// Declaring recursive functions
+let rec sumNumbers nfrom nto =   
+  if (nfrom > nto) then 0
+  else nfrom + (sumNumbers (nfrom + 1) nto)
+
+// Function that takes the 'aggregation operation' as an argument
+let rec aggregateNumbers initial op nfrom nto =   
+  if (nfrom > nto) then initial
+  else op nfrom (aggregateNumbers initial op (nfrom + 1) nto)
+
+// ----------------------------------------------------------------------------
+
+// Working with data in F#
+let data = [ 1 .. 10 ]
+let data0 = List.filter (fun n -> n%2 = 0) data
+let data1 = List.map (fun n -> n * 2) data0
+
+// Using the pipelining operator
+let res = 
+  data 
+  |> List.filter (fun n -> n%2 = 0) 
+  |> List.map (fun n -> n * 2)
+  |> List.length
