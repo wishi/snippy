b3b0528986bd827dd1067dd1636e4cf347207185
Marius <wishinet@gmail.com>
2009-11-15 16:07:29 +0100

    added solution

--- Java/NetWrk/HttpClient.java
+++ Java/NetWrk/HttpClient.java
@@ -8,19 +8,18 @@ public class HttpClient {
      * @param argv - defaults to localhost, server-name 
      * @throws ClassNotFoundException 
      */
-    public static void main(final String[] argv) throws IOException, ClassNotFoundException {
+    public static void main(final String[] argv) throws IOException {
 
        String url_name = argv.length == 1 ? argv[0] : "http://www.crazylazy.info/index.html";
                      
         try {
-            // gemäß SUNs Tutorial:
+            // gemaess SUNs Tutorial:
             // http://java.sun.com/docs/books/tutorial/networking/urls/readingWriting.html
                         
             URL url = new URL(url_name);
             URLConnection kaze = url.openConnection();
             
-            BufferedReader in = new BufferedReader(
-            new InputStreamReader(kaze.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(kaze.getInputStream()));
             String inputLine;
 
             // Output 
@@ -48,4 +47,5 @@ public class HttpClient {
     } // -- end of main
         
 
-}
+} 
+// EOF
